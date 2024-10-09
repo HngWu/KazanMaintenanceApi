3d30a4a42f5f2c0288dc87e1dbd8b66c934479d7
HngWu <hngwutan@gmail.com>
2024-10-09 15:48:49 +0800

    Update MaintenanceController.cs

--- KazanMaintenanceApi/Controllers/MaintenanceController.cs
+++ KazanMaintenanceApi/Controllers/MaintenanceController.cs
@@ -9,15 +9,60 @@ namespace KazanMaintenanceApi.Controllers
     {
         Wsc2019Session3FinalContext context = new Wsc2019Session3FinalContext();
 
-        [HttpGet]
+        [HttpGet("gettasks")]
         public IActionResult GetTasks()
         {
             
             try
             {
-                var tasks = context.Tasks
+                var tasks = context.Pmtasks
+                    .Select(x=> new
+                    {
+                        x.Asset.AssetName,
+                        x.Asset.AssetSn,
+                        x.Task.Name,
+                        scheduleType=x.PmscheduleType.Name,
+                        x.ScheduleDate,
+                        x.ScheduleKilometer,
+                        x.TaskDone
+
+                    })
                 .ToList();
                 return Ok(tasks);
+
+            }
+            catch (Exception)
+            {
+
+                return NotFound();
+            }
+        }
+
+        [HttpGet("getassets")]
+        public IActionResult GetAssets()
+        {
+            try
+            {
+                var assets = context.Assets
+                    .GroupJoin(context.AssetOdometers,
+                        asset => asset.Id,
+                        odometer => odometer.AssetId,
+                        (asset, odometers) => new
+                        {
+                            asset.Id,
+                            asset.AssetSn,
+                            asset.AssetName,
+                            asset.DepartmentLocationId,
+                            asset.EmployeeId,
+                            asset.AssetGroupId,
+                            asset.Description,
+                            asset.WarrantyDate,
+                            ReadDate = odometers.Select(o => o.ReadDate).FirstOrDefault(),
+                            OdometerAmount = odometers.Select(o => o.OdometerAmount).FirstOrDefault()
+                        })
+                    .ToList();
+
+                return Ok(assets);
 
             }
             catch (Exception)
